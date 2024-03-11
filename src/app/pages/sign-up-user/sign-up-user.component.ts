da67e142d9e32595453ac93f9f07f4ca186074a3
Youssef Hamed <yousiefhamed1@gmail.com>
2024-03-12 00:21:42 +0200

    adding the sign up user form

--- src/app/pages/sign-up-user/sign-up-user.component.ts
+++ src/app/pages/sign-up-user/sign-up-user.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sign-up-user',
   standalone: true,
-  imports: [CommonModule ,MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-up-user.component.html',
-  styleUrls: ['./sign-up-user.component.scss']
+  styleUrls: ['./sign-up-user.component.scss'],
 })
-export class SignUpUserComponent {
-
-
-
-}
+export class SignUpUserComponent {}
