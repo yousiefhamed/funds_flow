b69d5fb8c9447cef950ba97c7e0749c3be374431
Youssef Hamed <yousiefhamed1@gmail.com>
2024-06-11 13:50:49 +0300

    fixing viewport

--- src/app/pages/details-form/details-form.component.ts
+++ src/app/pages/details-form/details-form.component.ts
@@ -1,4 +1,3 @@
-import { MessageService } from 'primeng/api';
 import { MatSelectModule } from '@angular/material/select';
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -47,147 +46,156 @@ interface UploadEvent {
   providers: [],
 })
 export class DetailsFormComponent implements OnInit {
+  constructor(
+    private _ForminvestService: ForminvestService,
+    private _http: HttpClient,
+    // private messageService: MessageService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _categoriesService: CategoriesService,
+    private _ToastrService: ToastrService,
+    private _Renderer2: Renderer2
+  ) {}
 
-    constructor(
-      private _ForminvestService: ForminvestService,
-      private _http: HttpClient,
-      // private messageService: MessageService,
-      private _router:Router,
-      private _activatedRoute:ActivatedRoute ,
-      private  _categoriesService:CategoriesService,
-      private _ToastrService:ToastrService,
-      private _Renderer2:Renderer2
-    ) {}
-  
-    term: string = '';
-    file!: any;
-     formData = new FormData();
-     isLoading: boolean = false;
-    investUpdate:any;
-    done:number=0;
-    id:any;
- opportunitiesId!:string|null|number;
-    
-    details:Details[]=[];
+  term: string = '';
+  file!: any;
+  formData = new FormData();
+  isLoading: boolean = false;
+  investUpdate: any;
+  done: number = 0;
+  id: any;
+  opportunitiesId!: string | null | number;
 
+  details: Details[] = [];
 
+  _method: {} = {
+    _method: 'put',
+  };
 
-    _method:{}={
-  
-     _method:'put'
+  investForm: FormGroup = new FormGroup({
+    category_name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(15),
+    ]),
+    uuid: new FormControl('', []),
+    business_name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    amount_needed: new FormControl('', [Validators.required]),
+    // remaining_amount: new FormControl('',[Validators.required]),
+    potential_risks: new FormControl('', [Validators.required]),
+    future_growth: new FormControl('', [Validators.required]),
+    products_or_services: new FormControl('', [Validators.required]),
+    returns_percentage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(0),
+      Validators.maxLength(100),
+    ]),
+    company_valuation: new FormControl('', [Validators.required]),
+    start_date: new FormControl('', [Validators.required]),
+    end_date: new FormControl('', [Validators.required]),
+    revenues: new FormControl('', [Validators.required]),
+    net_profit: new FormControl('', [Validators.required]),
+    profit_margin: new FormControl('', [
+      Validators.required,
+      Validators.minLength(0),
+      Validators.maxLength(100),
+    ]),
+    cash_flow: new FormControl('', [Validators.required]),
+    ROI: new FormControl('', [Validators.required]),
+    photo: new FormControl('', [Validators.required]),
+  });
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.file = file;
+      this.formData.append('photo', file);
     }
-  
-    investForm: FormGroup = new FormGroup({
-      category_name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(15),
-      ]),
-      uuid: new FormControl('', [
-       
-      ]),
-      business_name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(20),
-      ]),
-      description: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-      amount_needed: new FormControl('', [Validators.required]),
-      // remaining_amount: new FormControl('',[Validators.required]),
-      potential_risks: new FormControl('', [Validators.required]),
-      future_growth: new FormControl('', [Validators.required]),
-      products_or_services: new FormControl('', [Validators.required]),
-      returns_percentage: new FormControl('', [
-        Validators.required,
-        Validators.minLength(0),
-        Validators.maxLength(100),
-      ]),
-      company_valuation: new FormControl('', [Validators.required]),
-      start_date: new FormControl('', [Validators.required]),
-      end_date: new FormControl('', [Validators.required]),
-      revenues: new FormControl('', [Validators.required]),
-      net_profit: new FormControl('', [Validators.required]),
-      profit_margin: new FormControl('', [
-        Validators.required,
-        Validators.minLength(0),
-        Validators.maxLength(100),
-      ]),
-      cash_flow: new FormControl('', [Validators.required]),
-      ROI: new FormControl('', [Validators.required]),
-      photo: new FormControl('', [Validators.required]),
-    });
-  
-  
-  
-  
-    onFileSelected(event: any) {
-      const file: File = event.target.files[0];
-  
-      if(file){
-        this.file= file
-       this.formData.append('photo', file);
-          }
-   
-    }
-  
-  
-    handelForm() {
-      this.isLoading = true;
-    
-  const formData1 = new FormData();
-  formData1.append('category_name',this.investForm.get('category_name')?.value)
-  formData1.append('business_name',this.investForm.get('business_name')?.value)
-  formData1.append('description',this.investForm.get('description')?.value)
-  formData1.append('amount_needed',this.investForm.get('amount_needed')?.value)
-  formData1.append('potential_risks',this.investForm.get('potential_risks')?.value)
-  formData1.append('future_growth',this.investForm.get('future_growth')?.value)
-  formData1.append('products_or_services',this.investForm.get('products_or_services')?.value)
-  formData1.append('uuid',this.investForm.get('uuid')?.value)
-  formData1.append('returns_percentage',this.investForm.get('returns_percentage')?.value)
-  formData1.append('company_valuation',this.investForm.get('company_valuation')?.value)
-  formData1.append('start_date',this.investForm.get('start_date')?.value)
-  formData1.append('end_date',this.investForm.get('end_date')?.value)
-  formData1.append('revenues',this.investForm.get('revenues')?.value)
-  formData1.append('net_profit',this.investForm.get('net_profit')?.value)
-  formData1.append('cash_flow',this.investForm.get('cash_flow')?.value)
-  formData1.append('profit_margin',this.investForm.get('profit_margin')?.value)
-  // formData1.append('id',this.id )
-  formData1.append('ROI',this.investForm.get('ROI')?.value)
-  formData1.append('photo',this.file)
-  console.log(this.file);
-  console.log(formData1);
-  console.log(this.investForm.value);
-  
-  let formData = this.investForm.value;
-  
-  formData.photo=this.file
-         
-    this._ForminvestService.updateForm(formData1 ,this.opportunitiesId).subscribe({
-      next: (res) => {
-        this.isLoading = false;
-        this._ToastrService.success(res.message)
-        window.location.reload()
-        
-      },
-      error: (err) => {
-        console.log(err);
-        this.isLoading = false;
-  
-      },
-    });
- 
-    
-  } 
-  
-  
-  
-  
-  getInvest(){
-  
-  
+  }
+
+  handelForm() {
+    this.isLoading = true;
+
+    const formData1 = new FormData();
+    formData1.append(
+      'category_name',
+      this.investForm.get('category_name')?.value
+    );
+    formData1.append(
+      'business_name',
+      this.investForm.get('business_name')?.value
+    );
+    formData1.append('description', this.investForm.get('description')?.value);
+    formData1.append(
+      'amount_needed',
+      this.investForm.get('amount_needed')?.value
+    );
+    formData1.append(
+      'potential_risks',
+      this.investForm.get('potential_risks')?.value
+    );
+    formData1.append(
+      'future_growth',
+      this.investForm.get('future_growth')?.value
+    );
+    formData1.append(
+      'products_or_services',
+      this.investForm.get('products_or_services')?.value
+    );
+    formData1.append('uuid', this.investForm.get('uuid')?.value);
+    formData1.append(
+      'returns_percentage',
+      this.investForm.get('returns_percentage')?.value
+    );
+    formData1.append(
+      'company_valuation',
+      this.investForm.get('company_valuation')?.value
+    );
+    formData1.append('start_date', this.investForm.get('start_date')?.value);
+    formData1.append('end_date', this.investForm.get('end_date')?.value);
+    formData1.append('revenues', this.investForm.get('revenues')?.value);
+    formData1.append('net_profit', this.investForm.get('net_profit')?.value);
+    formData1.append('cash_flow', this.investForm.get('cash_flow')?.value);
+    formData1.append(
+      'profit_margin',
+      this.investForm.get('profit_margin')?.value
+    );
+    // formData1.append('id',this.id )
+    formData1.append('ROI', this.investForm.get('ROI')?.value);
+    formData1.append('photo', this.file);
+    console.log(this.file);
+    console.log(formData1);
+    console.log(this.investForm.value);
+
+    let formData = this.investForm.value;
+
+    formData.photo = this.file;
+
+    this._ForminvestService
+      .updateForm(formData1, this.opportunitiesId)
+      .subscribe({
+        next: (res) => {
+          this.isLoading = false;
+          this._ToastrService.success(res.message);
+          window.location.reload();
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+        },
+      });
+  }
+
+  getInvest() {
     this._categoriesService.getDetails(this.opportunitiesId).subscribe({
       next: (res) => {
         this.isLoading = false;
